a3bb16a3337fdbc1ed6efd7ea1438e7c4f78d918
Noah Bar <noahbar@gmail.com>
2018-04-18 12:38:34 +0200

    tabs are added

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,7 +1,9 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,14 +13,20 @@ import android.support.v4.app.FragmentPagerAdapter;
  * each list item based on a data source which is a list of {@link Word} objects.
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
+
+    //?? כדי להוסיף strings
+    /** Context of the app */
+    private Context mContext;
+
     /**
      * Create a new {@link CategoryAdapter} object.
      *
      * @param fm is the fragment manager that will keep each fragment's state in the adapter
      *           across swipes.
      */
-    public CategoryAdapter(FragmentManager fm) {
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     /**
@@ -43,5 +51,19 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 }
